eb82f3079d292d5193a4a1f34bed7b7b26dcd64c
Andrew Ayetey <ayeteya@gmail.com>
2019-12-02 11:15:15 -0600

    first commit

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let addFunction = function(x, y){
+    return x + y
+}
+let subtractFunction = function(x, y){
+    return x - y
+}
+let multiplyFunction = function(x, y){
+    return x * y
+}
+let divideFunction = function(x, y){
+    return x / y
+} 
