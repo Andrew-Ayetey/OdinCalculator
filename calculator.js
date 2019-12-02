ae5e8bd9bd753663a1ae1ce5dc5dbdb05dbac84d
Andrew Ayetey <ayeteya@gmail.com>
2019-12-02 12:55:18 -0600

    added html and css

--- calculator.js
+++ calculator.js
@@ -10,3 +10,22 @@ let multiplyFunction = function(x, y){
 let divideFunction = function(x, y){
     return x / y
 } 
+let operator = function(operate, value1, value2){
+    switch(operate){
+    case "+":
+        return value1 + value2
+        break;
+    case "-":
+        return value1 - value2
+        break;
+    case "/":
+        return value1 / value2
+        break;
+    case "*":
+        return value1 * value2
+        break;
+    default:
+        break;
+
+    }
+}
