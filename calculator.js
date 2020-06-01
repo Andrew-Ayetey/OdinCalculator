d77f5145467f09a02559d95f74a6acb04d2ce96e
Andrew Ayetey <ayeteya@gmail.com>
2020-06-01 17:09:38 -0500

    worked on PEMDAS

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+//functions
+
 let addFunction = function(x, y){
     return x + y
 }
@@ -26,6 +28,56 @@ let operatorFunction = function(operate, value1, value2){
     }
 }
 
+function computeArray(storageArray){
+    let firstVariable = Number(storageArray[0])
+    console.log(firstVariable)
+    let secondVariable = Number(storageArray[2])
+    let operation = storageArray[1]
+    let output = operatorFunction(operation, firstVariable, secondVariable)
+    return output
+}
+
+function PEMDASparser(array){
+    newArray = []
+    for (i = 0; i < array.length; i++){
+        if (array[i] == "*"){
+
+
+            console.log(array.slice(0, i-2))
+            let before = array.slice(0, i-1)
+            let after = array.slice(i+2)
+            console.log(before, after)
+            newArray.push(before, operatorFunction(array[i],array[i-1],array[i+1]), after)
+        }
+    }
+//         for (i = 0; i < array.length; i++){
+//             if (array[i] == "/"){
+//                 console.log(i)
+//                 console.log(array[i-1], array[i+1])
+//                 newArray.push(array[i], array[i+1])
+//             }
+        
+//     }
+//     for (i = 0; i < array.length; i++){
+//         if (array[i] == "+"){
+//             console.log(i)
+//             console.log(array[i-1], array[i+1])
+//             newArray.push(array[i], array[i+1])
+//         }
+    
+// }
+// for (i = 0; i < array.length; i++){
+//     if (array[i] == "-"){
+//         console.log(i)
+//         console.log(array[i-1], array[i+1])
+//         newArray.push(array[i], array[i+1])
+//     }
+
+// }
+    console.log(newArray)
+    return newArray
+    
+}
 //objects
 function operation(firstVariable, operator, secondVariable){
     operationsList = ["+", "-", "/", "*"]
@@ -54,6 +106,7 @@ let checkStorageArray = function(array){
 //Elements and Variables
 
 let storageArray = []
+let historyArray = []
 let operationsArray = ["+", "-", "/", "*"]
 
 let numbersButtons = document.querySelectorAll(".number")
@@ -82,6 +135,7 @@ for(var i = 0; i < 10; i++){
             numberDisplay.innerHTML += innerId
             numberDisplayValue = numberDisplay.innerHTML
             storageArray = numberDisplayValue.split(" ")
+            
         }
     })
     
@@ -105,6 +159,7 @@ for(var i = 0; i < operatorButtons.length; i++){
             numberDisplayValue = numberDisplay.innerHTML
             storageArray = numberDisplayValue.split(" ")
             storageArray.pop()
+            historyArray.push(storageArray)
             
 
             
@@ -117,23 +172,48 @@ for(var i = 0; i < operatorButtons.length; i++){
 clearEvent.addEventListener('click', e =>{
     numberDisplay.innerHTML = 0
     numberDisplayValue = 0
+    storageArray = []
 })
 
 //equals
 equalsEvent.addEventListener('click', e =>{
-    if (numberDisplayValue == 0){
+    if (storageArray.length < 3){
         return
     }
-    else{
-    numberDisplay.innerHTML = "Temp"
-    numberDisplayValue = 0
+    else {
+        if (storageArray.length % 2 == 0){
+            return
+        }
+        else{
+            
+            storageArray = PEMDASparser(storageArray)
+            while(storageArray.length > 3){
+                let newValue = computeArray(storageArray.slice(0,3))
+                
+                storageArray.shift()
+                storageArray.shift()
+                storageArray.shift()
+                storageArray.unshift(Number(newValue))
+                historyArray.push(storageArray)
+                console.log(storageArray)
+                console.log(historyArray)
+            }
+            let output = computeArray(storageArray)
+            numberDisplay.innerHTML = output
+            numberDisplayValue = "" + output
+            historyArray.push(storageArray)
+            historyArray.push(output)
+        }
     }
+    
 })
 
 
 
 
 // testing
+
+let testArray = [12, "+", "7", "-", "5", "*", "6", "/", "2"]
 
 
 
